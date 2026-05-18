27b2644443bc5df6fa17b7cd7a4388f7083df094
Emily Middleton <emiddleton@smith.edu>
2026-05-18 15:44:27 -0500

    feat: clear out vite default and implement router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+
+import App from './App.jsx';
+import Home from './pages/Home.jsx';
+import StudentDirectory from './pages/StudentDirectory.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "/student-directory",
+        element: <StudentDirectory />
+      }
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
