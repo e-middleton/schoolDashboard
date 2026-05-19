228ab7956cbe4da0794fe89855f537e088630271
Yuwen Zhang <yuwen.x.zhang@gmail.com>
2026-05-18 19:41:06 -0700

    chore: Installed mui icon dependencies

--- src/pages/ClassDirectory.jsx
+++ src/pages/ClassDirectory.jsx
@@ -1,10 +1,16 @@
 import "../styling/ClassDirectory.css"
+
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
+
 import Button from '@mui/material/Button';
+
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+
 
 const ClassDirectory = () => {
 
@@ -23,6 +29,16 @@ const ClassDirectory = () => {
       "name": "[Class name]",
       "teacher": "Teacher name",
       "image": ""
+    },
+        {
+      "name": "[Class name]",
+      "teacher": "Teacher name",
+      "image": ""
+    },
+        {
+      "name": "[Class name]",
+      "teacher": "Teacher name",
+      "image": ""
     }
   ]
 
@@ -32,16 +48,20 @@ const ClassDirectory = () => {
       <h1>Class Directory</h1>
 
       {/* Search bar and add, delete buttons */}
-      <p>Search bar</p>
+      <SearchIcon />
+      <InputBase
+        placeholder="Search…"
+        inputProps={{ 'aria-label': 'search' }}
+      />
       <div className="classbuttons-div">
-        <p>Add Class</p>
-        <p>Delete Class</p>
+        <Button sx={{"background-color": "#11578A", "color": "white"}} variant="contained">Add Class</Button>
+        <Button sx={{"background-color": "#11578A", "color": "white"}} variant="contained">Delete Class</Button>
       </div>
+
 
       {/* Grid of all classes */}
       <div className="classlist-div">
         {classData.map(aClass => (
-
           // Card background
           <Card sx={{ minWidth: 380 }}>
 
@@ -49,30 +69,25 @@ const ClassDirectory = () => {
               
               {/* Card text and button */}
               <CardContent sx={{padding: "2rem"}}>
-
                 <Typography variant="h5" component="div">
                   {aClass.name}
                 </Typography>
-
                 <Typography variant="body2" sx={{ color: 'text.secondary' }}>
                   {aClass.teacher}
                 </Typography>
-
                 <Button sx={{"background-color": "#11578A", "color": "white"}} variant="contained">View Dashboard</Button>
-
               </CardContent>
               
               {/* Card image */}
               <CardMedia
                 component="img"
-                height="200"
+                height="220"
                 image={aClass.image || "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/330px-Placeholder_view_vector.svg.png?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=thumbnail"}
                 alt={`Image for ${aClass.name} class`}
                 sx={{"width": 120}}
               />
             </CardActionArea>
           </Card>
-
         ))}
       </div>
     </>
