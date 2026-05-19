ca926ec12afde74da2a024200398db584a39ca22
Yuwen Zhang <yuwen.x.zhang@gmail.com>
2026-05-18 18:34:37 -0700

    chore: Added mock class data

--- src/pages/ClassDirectory.jsx
+++ src/pages/ClassDirectory.jsx
@@ -1,7 +1,23 @@
 const ClassDirectory = () => {
+
+  const classData = [
+    {
+      "name": "G3 Math",
+      "teacher": "Ms. Anne Randoll"
+    },
+    {
+      "name": "G3 Math",
+      "teacher": "Mr. Greg Gonzales"
+    },
+    {
+      "name": "[Class name]",
+      "teacher": "Teacher name"
+    }
+  ]
+
   return (
     <>
-      Class Directory
+      <h1>Class Directory</h1>
     </>
   )
 };
