8825de987afe7722a678c88dc66da14c43d7a4f3
SSsunnyH <sunshinehuang2004@gmail.com>
2026-05-19 12:54:44 -0400

    Added class data structure

---
+++ src/utils/classes.js
@@ -0,0 +1,12 @@
+const classes = [{
+    id: 1,
+    className: "1stGradeHistory",
+    teacherID: "teacher001",
+    studentID: [
+        "student001", 
+        "student002", 
+        "student003",
+        "student004",
+        "student005",
+    ]
+}]
