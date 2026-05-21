ce11f8985c37b731725482f89ca8aaa318ef7571
Yuwen Zhang <yuwen.x.zhang@gmail.com>
2026-05-21 08:41:20 -0700

    feat: implemented add new grade functionality, enforces unique assignment name

--- src/pages/GradeManagement.jsx
+++ src/pages/GradeManagement.jsx
@@ -1,5 +1,7 @@
 import "../styling/GradeManagement.css"
-import { fetchGrades, addGrade, updateGrade, deleteGrade } from "../utils/grades.js"
+import { fetchGradeDocument, createGradeDocument } from "../utils/grades.js"
+
+import GradeForm from "../components/GradeForm.jsx"
 
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router'
@@ -8,82 +10,70 @@ import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 
 // Icons
-// import SearchIcon from '@mui/icons-material/Search';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import EditIcon from '@mui/icons-material/Edit';
-import SendIcon from '@mui/icons-material/Send';
 
 // Accordian
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 
-// Form inputs
-import Select from '@mui/material/Select';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import TextField from '@mui/material/TextField';
-
-import { styled } from '@mui/material/styles';
-
 const GradeManagement = () => {
   
   /*
   todo:
   - refactor components
-      - removed unused mui imports
+      - (done) removed unused mui imports
   - add comments
 
   - interactive
     - dynamically show category and grades
-    - dynamically show category in options for new assignment]
-    - show and hide new assignment
+    - (done) dynamically show category in options for new grade
+    - (done) show and hide new grade
   - validate grade input; error message
+    - make all fields required
     - prevent typing non-numbers
+    - assignment name
+  - success state, clear form, if time: open accordian and scroll down
 
+  ----------
+  - edit grade
+  - delete grade
   - navigation: params, back button (class page), need to know which class came from
   -----------
   - ignore: grade calculation algorithm
   if time: add search bar for assignments (?)
   */
 
+  const params = useParams(); // {classID: 01, studentID: 01}
+
   // Grades for the student and class being viewed
   const [gradeData, setGradeData] = useState({});
-
-  // New Assignment Form Input Values
-  const [assignmentFormInput, setAssignmentFormInput] = useState({
-    assignmentName: "",
-    assignmentGrade: "",
-    assignmentCategory: ""
-  })
-
-  console.log(assignmentFormInput);
+  const [refreshToggle, setRefreshToggle] = useState(false); // triggers refresh on data update
 
   // States controlling displayed content
-  const [newAssignmentFormShown, setNewAssignmentFormShown] = useState(false);
-
-  // console.log(assignmentName)
-  // console.log(assignmentGrade)
-  // console.log(assignmentCategory)
-
-  // Get route parameters
-  const params = useParams(); // {classID: 01, studentID: 01}
+  const [gradeFormShown, setGradeFormShown] = useState(false);
+  const [message, setMessage] = useState("");
 
   // Fetch grade data for class and student viewed
   useEffect(() => {
     const getData = async () => {
-      const data = await fetchGrades(params.classID, params.studentID)
+      const data = await fetchGradeDocument(params.classID, params.studentID)
       setGradeData(data);
     }
     getData();
-  }, [])
-  // console.log(gradeData);
+  }, [refreshToggle])
+
+  // console.log("params");
+  // console.log(params);
+
+  // console.log("grade document");
+  console.log(gradeData);
   
   return (
     <Grid container spacing={10} columns={12} sx={{"display": "flex", "justifyContent": "center"}}>
@@ -100,81 +90,28 @@ const GradeManagement = () => {
         </Grid>
 
 
-        {/* New Assignment Form */}
-        {newAssignmentFormShown && 
-        <Grid container sx={{"bgcolor": "#F5F3E4", padding: "4rem 2rem", "borderRadius": 2, "width": "auto", "display": "flex", "justifyContent": "space-between", "flexDirection": "column"}}>
-         
-         {/* Top */}
-          <Grid container sx={{"display": "flex", "justifyContent": "space-between"}}>
-            {/* Header */}
-            <h2>New Assignment Grading</h2>
-
-            {/* Hide Form Button */}
-            <Button sx={{"backgroundColor": "#CE2626", "color": "white"}} variant="contained" startIcon={<DeleteIcon />}>Cancel</Button>
-          </Grid>
-
-          {/* Assignment Name Input */}
-          <Grid container direction="column" spacing={1}>
-            <InputLabel required="true" sx={{"fontSize": "1rem", color: "black"}}>Assignment Name</InputLabel>
-            <TextField
-              placeholder="Enter name..."
-              value={assignmentFormInput.assignmentName}
-              onChange={(e) => setAssignmentFormInput({...assignmentFormInput, assignmentName: e.target.value})}
-              sx={{"flexGrow": "1", "bgcolor": "#FFFDEB", "borderRadius": 1}}
-            />
-          </Grid>
-          
-          {/* Grade Input */}
-          <Grid container direction="column" spacing={1}>
-            <Grid container direction="row" sx={{"display": "flex", "alignItems": "end"}}>
-              <InputLabel required="true" sx={{"fontSize": "1rem", color: "black"}}>Grade</InputLabel>
-              <Typography variant="body2" sx={{"display": "inline", color: "#858585"}}> (out of 100)</Typography>
-            </Grid>
-            <Grid columns={12} sx={{"display": "flex", "flexDirection": "row", "alignItems": "center"}}>
-              <TextField
-                placeholder="Between 0 and 100"
-                value={assignmentFormInput.assignmentGrade}
-                onChange={(e) => setAssignmentFormInput({...assignmentFormInput, assignmentGrade: e.target.value})}
-                sx={{"flexGrow": "1", "bgcolor": "#FFFDEB", "borderRadius": 1, width: 1/10}}
-              />
-            </Grid>
-          </Grid>
-          
-          {/* Category select input */}
-          <Grid container direction="column" spacing={1}>
-            <InputLabel required="true" sx={{"fontSize": "1rem", color: "black"}}>Category</InputLabel>
-            <Select
-              value={assignmentFormInput.assignmentCategory} //update to set to 1st category
-              displayEmpty
-              renderValue={(value) => {
-                if (!value) {
-                  return <Typography sx={{color:"#929292"}}>Select...</Typography>;
-                }
-                return <>{value}</>;
-              }}
-              onChange={(e) => setAssignmentFormInput({...assignmentFormInput, assignmentCategory: e.target.value})}
-              sx={{"width": 1/3}}
-            >
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
-          </Grid>
-
-          {/* Submit Button */}
-          <Button sx={{"backgroundColor": "#11578A", "color": "white", "width": 1/3}} endIcon={<SendIcon />} variant="contained">Submit </Button>
-        </Grid>
+        {/* Grade Form */}
+        {gradeFormShown && 
+          <GradeForm closeForm={() => {setGradeFormShown(false)}}
+            refreshToggle={refreshToggle}
+            setRefreshToggle={setRefreshToggle}
+            message={message}
+            setMessage={setMessage}
+          />
         }
 
-
-
-
-        {/* Add Assignments Button */}
-        <Button sx={{"backgroundColor": "#11578A", "color": "white", "width": 1/3}} variant="contained" endIcon={<AddIcon />}
-          onClick={() => setNewAssignmentFormShown(true)}
-          >Add Assignment</Button>
-
-
+        {/* Add Grade Button */}
+        {!gradeFormShown &&
+          <>
+            <Button sx={{"backgroundColor": "#11578A", "color": "white", "width": 1/3}} variant="contained" endIcon={<AddIcon />}
+            onClick={() => setGradeFormShown(true)}>
+              Add Grade
+            </Button>
+            {
+              message && <Typography variant="body2" sx={{color: "green"}}>{message}</Typography>
+            }
+          </>
+        }
 
         {/* List of Accordians */}
         <Grid container spacing={1}>
@@ -203,29 +140,6 @@ const GradeManagement = () => {
                 <Grid container space={1}>
                 </Grid>
               </Grid>
-            </AccordionDetails>
-
-          </Accordion>
-
-          {/* Accordian Row */}
-          <Accordion disableGutters
-            sx={{
-              "width": "100%",
-              "backgroundColor": "#FFFDEB", 
-              "borderRadius": 2
-            }}>
-
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-            >
-              <Typography component="span">Quizzes</Typography>
-            </AccordionSummary>
-
-            <AccordionDetails>
-              <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                malesuada lacus ex, sit amet blandit leo lobortis eget.
-              </Typography>
             </AccordionDetails>
 
           </Accordion>
--- src/utils/grades.js
+++ src/utils/grades.js
@@ -1,60 +1,95 @@
 import { doc, addDoc, deleteDoc, updateDoc, collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebase.js";
 
-/*
-grades:
-{
-  classID: 01
-  studentID: 01
-  participationGrades: [
-    {
-      day1: 100
-    }
-  ]
-  quizGrades: [
-    {
-      ...
-    }
-  ]
-  testGrades...
-  projectGrades...
+// Fixed grade categories
+const gradeCategories = ["Quizzes", "Participation", "Projects", "Tests"];
+
+// Structure of grade document
+const gradeStructure = {
+  classID: "",
+  studentID: "",
+  participationGrades: [],
+  quizGrades: [], // contains grade records ex. {assignmentName: 'assignment1', assignmentGrade: 98}
+  testGrades: [], // contains grade records
+  projectGrades: [] // contains grade records
 }
-*/
 
+// Maps category to designated property in document
+const categoryToProperty = {
+  "Participation": "participationGrades",
+  "Quizzes": "quizGrades",
+  "Tests": "testGrades",
+  "Projects": "projectGrades"
+}
 
-
-// retrieve grade records from the database
-  // for a specified **class and **student
-const fetchGrades = async (targetClassID, targetStudentID) => {
+// retrieve all grade records from the database for a specified **class and **student
+const fetchGradeDocument = async (targetClassID, targetStudentID) => {
+  console.log("fetching grade document");
   const querySnapshot = await getDocs(collection(db, "grades"));
-  const parsedSnapshot = querySnapshot.docs.map((doc) => ({...doc.data()}));
+  const parsedSnapshot = querySnapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}));
 
-  // Filter by class and student
-  return parsedSnapshot.filter((grade) => 
-    grade.classID === targetClassID && grade.studentID === targetStudentID);
+  console.log(parsedSnapshot);
+
+  // Search by class and student
+  const matchingGradeDocument = parsedSnapshot.find((grade) => 
+    grade.classID == targetClassID && grade.studentID == targetStudentID);
+  return matchingGradeDocument;
 };
 
-// add a new grade to the database
 
-const addGrade = async (grade) => {
-//   await addDoc(collection(db, "grades", classID), {
-//     classID: grade.classID,
-//     studentID: grade.studentID,
-//     quizGrades: grade.quizGrades,
-//     projectGrades: grade.projectGrades,
-//     participationGrades: grade.participationGrades,
-//     testGrades: grade.testGrades
-//   })
+// create initial grade document
+const createGradeDocument = async (targetClassID, targetStudentID) => {
+  // if time: check that record doesn't already exist -> if does, prevent create
+
+  await addDoc(collection(db, "grades"), {
+    ...gradeStructure,
+    classID: targetClassID,
+    studentID: targetStudentID
+  })
 }
 
-// -----------
+// add a new record to the grade document for a specified **class and **student
+    // -> returns false if assignmentName already exists
+const addGradeRecord = async (gradeRecord) => {
+  // fetch all records matching selected class and student
+  const gradeDocument = await fetchGradeDocument(gradeRecord.classID, gradeRecord.studentID);
+  // initial grade document doesn't exist
+  if(!gradeDocument) {
+    console.log("error: initial grade document was never created")
+  }
 
-const updateGrade = async () => {
+  // search for record with matching assignment name
+  const categoryListName = categoryToProperty[gradeRecord.assignmentCategory];
+  const matchingCategoryList = gradeDocument[categoryToProperty[gradeRecord.assignmentCategory]];
+  const existingRecord = matchingCategoryList.some((record) => (
+    record.assignmentName === gradeRecord.assignmentName
+  )); 
+
+  // no record exists with same name -> valid
+  if (!existingRecord) {
+    gradeDocument[categoryListName] = [
+      ...matchingCategoryList,
+      {
+        assignmentName: gradeRecord.assignmentName,
+        assignmentGrade: gradeRecord.assignmentGrade
+      }
+    ]
+    await updateDoc(doc(db, "grades", gradeDocument.id), gradeDocument)
+    return true;
+  }
+
+  console.log(`add failed: record with name ${gradeRecord.name} already exists`);
+  return false;
+}
+
+// update a specified grade record
+const updateGradeRecord = async (gradeRecord) => {
+}
+
+// delete a specified grade record
+const deleteGradeDocument = async () => {
 
 }
 
-const deleteGrade = async () => {
-
-}
-
-export { fetchGrades, addGrade, updateGrade, deleteGrade };
+export { gradeCategories, 
+  fetchGradeDocument, createGradeDocument, addGradeRecord, updateGradeRecord, deleteGradeDocument };
