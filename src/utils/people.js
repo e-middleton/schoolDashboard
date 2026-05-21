a40aeea1b340d95cf9c22f28fe7e4ac76976b203
Emily Middleton <emiddleton@smith.edu>
2026-05-21 12:01:18 -0500

    feat: date of birth crud

--- src/utils/people.js
+++ src/utils/people.js
@@ -1,10 +1,11 @@
-import { doc, addDoc, deleteDoc, updateDoc, collection, getDocs } from "firebase/firestore";
+import { doc, addDoc, deleteDoc, updateDoc, collection, getDocs, Timestamp } from "firebase/firestore";
 import {db} from "../../firebase.js";
+import dayjs from 'dayjs';
 
 // retrieve all teacher records from the database
 const fetchAllPeople = async (collectionName) => {
   const querySnapshot = await getDocs(collection(db, collectionName));
-  return querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+  return querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data(), dateOfBirth: dayjs(doc.data().dateOfBirth.toDate())}));
 };
 
 // add a new person to the database
@@ -13,23 +14,14 @@ const addPerson = async (collectionName, person, role) => {
     firstName: person.firstName,
     lastName: person.lastName,
     classIDs: [],
-    role: role
+    role: role,
+    dateOfBirth: Timestamp.fromDate(person.dateOfBirth.toDate())
   })
 }
 
 const updatePerson = async (collectionName, person) => {
-  // classes is usually an array of objects, so check then map if necessary
-  if (typeof(person.classes) === "string"){
-    await updateDoc(doc(db, collectionName, person.id), person); // ideally should include the updated field
-  } else {
-    // separate out classIDs
-    const classIDs = person.classes.map((item) => {
-      return item.id;
-    })
-
-    person.classes = classIDs;
-    await updateDoc(doc(db, collectionName, person.id), person); // ideally should include the updated field
-  }
+  person.dateOfBirth = Timestamp.fromDate(person.dateOfBirth.toDate())
+  await updateDoc(doc(db, collectionName, person.id), person); // ideally should include the updated field
 }
 
 const deletePerson = async (collectionName, person) => {
