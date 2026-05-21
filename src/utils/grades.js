478282f1b17c75813f73237d9d999c7b5d1521c1
Yuwen Zhang <yuwen.x.zhang@gmail.com>
2026-05-20 18:30:24 -0700

    chore: created skeleton for grades util

---
+++ src/utils/grades.js
@@ -0,0 +1,23 @@
+import { doc, addDoc, deleteDoc, updateDoc, collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase.js";
+
+// retrieve grade records from the database
+  // for a specified **class and **student
+const fetchGrades = async () => {
+  
+};
+
+// add a new grade to the database
+const addGrade = async () => {
+
+}
+
+const updateGrade = async () => {
+
+}
+
+const deleteGrade = async () => {
+
+}
+
+export { fetchGrades, addGrade, updateGrade, deleteGrade };
