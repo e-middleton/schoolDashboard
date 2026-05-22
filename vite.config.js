aa081956ca6310f78543d3f56bed9f9ac2d98da8
Emily Middleton <emiddleton@smith.edu>
2026-05-21 19:17:14 -0500

    feat: server functionality for aws connection

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Forward any request starting with /get-view-url or /generate-upload-url
+      '/get-view-url': 'http://localhost:3001', 
+      '/generate-upload-url': 'http://localhost:3001'
+    }
+  }
+  
 })
